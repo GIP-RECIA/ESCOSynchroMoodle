dd32365d53f8f5e97cf2815d569ed5273ff4ffb7
Victor CASTRO-CINTAS <victor.castro-cintas@gfi.fr>
2019-02-13 14:55:28 +0100

    Utilisation du nouveau logger

--- src/synchromoodle/__main__.py
+++ src/synchromoodle/__main__.py
@@ -4,7 +4,7 @@
 Entrypoint
 """
 
-import logging
+from logging import getLogger
 import sys
 
 from synchromoodle import actions
@@ -22,22 +22,20 @@ def main():
     config = config_loader.load(['config.yml', 'config.yaml'], True)
 
     config = config_loader.update(config, arguments.config)
-
-    # TODO: Rendre le logger configurable, et ajouter un handler par logger (établissement, élève, enseignant)
-    logging.basicConfig(format="%(levelname)s:%(message)s", stream=sys.stdout, level=logging.DEBUG)
+    log = getLogger()
 
     for action in config.actions:
         try:
             action_func = getattr(actions, action)
         except AttributeError:
-            logging.error("Action invalide: %s", action)
+            log.error("Action invalide: %s", action)
             continue
-        logging.info('Démarrage de l\'action "%s"' % action)
+        log.info('Démarrage de l\'action "%s"' % action)
         try:
             action_func(config, arguments)
         except Exception as e:
-            logging.exception("Une erreur inattendue s'est produite")
-        logging.info('Fin de l\'action "%s"' % action)
+            log.exception("Une erreur inattendue s'est produite")
+        log.info('Fin de l\'action "%s"' % action)
 
 
 if __name__ == "__main__":
