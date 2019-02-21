a67891dc64170914dfec4fbd1b39cb286abf7a8f
Victor CASTRO-CINTAS <victor.castro-cintas@gfi.fr>
2019-02-21 13:57:37 +0100

    Correction des resultats attendus sur les filtres de timestamp

--- test/test_ldaputils.py
+++ test/test_ldaputils.py
@@ -102,9 +102,9 @@ def test_get_filtre_eleves():
            "(&(objectClass=ENTEleve))"
     assert ldaputils._get_filtre_eleves(uai="some-uai") == "(&(objectClass=ENTEleve)(ESCOUAI=some-uai))"
     assert ldaputils._get_filtre_eleves(since_timestamp=datetime_value) == \
-        "(&(objectClass=ENTEleve)(modifyTimeStamp>=2019-04-09T21:42:01))"
+        "(&(objectClass=ENTEleve)(modifyTimeStamp>=20190409214201Z))"
     assert ldaputils._get_filtre_eleves(since_timestamp=datetime_value, uai="other-uai") == \
-        "(&(objectClass=ENTEleve)(ESCOUAI=other-uai)(modifyTimeStamp>=2019-04-09T21:42:01))"
+        "(&(objectClass=ENTEleve)(ESCOUAI=other-uai)(modifyTimeStamp>=20190409214201Z))"
 
 
 def test_get_filtre_etablissement():
@@ -118,16 +118,16 @@ def test_get_filtre_etablissement():
 def test_get_filtre_personnes():
     assert ldaputils._get_filtre_personnes(datetime_value) == \
            "(&(|(objectClass=ENTPerson))(!(uid=ADM00000))" \
-           "(modifyTimeStamp>=2019-04-09T21:42:01))"
+           "(modifyTimeStamp>=20190409214201Z))"
     assert ldaputils._get_filtre_personnes(datetime_value, foo="bar", hello=["world", "dude"]) in \
         ["(&(|(objectClass=ENTPerson))"
          "(!(uid=ADM00000))"
          "(|(foo=bar)(hello=world)(hello=dude))"
-         "(modifyTimeStamp>=2019-04-09T21:42:01))",
+         "(modifyTimeStamp>=20190409214201Z))",
          "(&(|(objectClass=ENTPerson))"
          "(!(uid=ADM00000))"
          "(|(hello=world)(hello=dude)(foo=bar))"
-         "(modifyTimeStamp>=2019-04-09T21:42:01))"]
+         "(modifyTimeStamp>=20190409214201Z))"]
 
 
 def test_get_filtre_enseignants():
@@ -141,4 +141,4 @@ def test_get_filtre_enseignants():
         ")" \
         "(!(uid=ADM00000))" \
         "(ESCOUAI=UAI00000)" \
-        "(modifyTimeStamp>=2019-04-09T21:42:01))"
+        "(modifyTimeStamp>=20190409214201Z))"
