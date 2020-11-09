16f8323c62a9291feb1cbb80cb7d12e8787263d0
Pierre Legay <pierre.legay@recia.fr>
2020-11-09 15:04:27 +0100

    Ajout profile : ‘National_ETA’ et ‘National_EVS’ pour BBB

--- synchromoodle/synchronizer.py
+++ synchromoodle/synchronizer.py
@@ -383,7 +383,7 @@ class Synchronizer:
         else:
             if etablissement_context.structure_ldap.type.startswith('LYCEE') \
                     or etablissement_context.structure_ldap.type == self.__config.constantes.type_structure_ens_adapte:
-                if set(enseignant_ldap.profils).intersection(['National_ENS','National_DOC','National_DIR']):
+                if set(enseignant_ldap.profils).intersection(['National_ENS','National_DOC','National_DIR', 'National_ETA', 'National_EVS']):
                     log.info(" 	AJOUT ROLE BIGBLUEBUTTON %s" % id_user)
                     self.__db.add_role_to_user(self.__config.constantes.id_role_bigbluebutton,
                                                etablissement_context.id_instance_moodle, id_user)
