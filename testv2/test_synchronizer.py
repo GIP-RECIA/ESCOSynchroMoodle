42e0909d80dff822fa56489de8b7a7328813031c
Nathan Cailbourdin <nathan.cailbourdin@recia.fr>
2023-04-17 11:00:16 +0200

    Tests sur les enseignants

--- testv2/test_synchronizer.py
+++ testv2/test_synchronizer.py
@@ -3,6 +3,8 @@ Module de tests de la synchronisation, le principe est le suivant :
 - Insertion de données de test dans la base de données moodle (donc pas présentes dans le LDAP)
 - Lancement du script de synchronisation
 - Vérification des modifications/suppressions sur les données qui avaient été insérées
+Les fonctions de test suivent la convention de nommage définie ci-dessous :
+test_[type utilisateur]_[comportement attendu]_[délai de connexion]_[cas testé]
 """
 
 import pytest
@@ -13,8 +15,6 @@ from synchromoodle import actions
 from argparse import ArgumentParser,Namespace
 from synchromoodle.config import ConfigLoader,Config
 from synchromoodle.webserviceutils import WebService
-
-SECONDS_PER_DAY = 86400
 
 @pytest.fixture(scope="module", name="temp")
 def temp():
@@ -87,77 +87,11 @@ def db(config: Config, arguments: Namespace, temp: dict[str, list[int]], webserv
     #Suppression des utilisateurs de test
     for userid in temp["eleves"]:
         remove_fake_user(db, userid)
+    for userid in temp["profs"]:
+        remove_fake_user(db, userid)
 
     db.connection.commit()
     db.disconnect()
-
-
-def insert_eleves(db: Database, config: Config, arguments: Namespace, temp: dict[str, list[int]], webservice: WebService):
-    """
-    Insérère toutes les données nécéssaisres aux tests
-    des élèves dans la base de données moodle
-    """
-    #Récupération du timestamp actuel
-    now = db.get_timestamp_now()
-
-    #Insertion des utilisateurs de test (élèves)
-    eleveid_a = insert_fake_user(db, "F1700tsa", "test", "A", "test.A@netocentre.fr", 2, "0290009c")
-    eleveid_b = insert_fake_user(db, "F1700tsb", "test", "B", "test.B@netocentre.fr", 2, "0290009c")
-    eleveid_c = insert_fake_user(db, "F1700tsc", "test", "C", "test.C@netocentre.fr", 2, "0290009c")
-    eleveid_d = insert_fake_user(db, "F1700tsd", "test", "D", "test.D@netocentre.fr", 2, "0290009c")
-    eleveid_e = insert_fake_user(db, "F1700tse", "test", "E", "test.E@netocentre.fr", 2, "0290009c")
-    eleveid_f = insert_fake_user(db, "F1700tsf", "test", "F", "test.F@netocentre.fr", 2, "0290009c")
-    eleveid_g = insert_fake_user(db, "F1700tsg", "test", "G", "test.G@netocentre.fr", 2, "0290009c")
-    eleveid_h = insert_fake_user(db, "F1700tsh", "test", "H", "test.H@netocentre.fr", 2, "0290009c")
-    eleveid_i = insert_fake_user(db, "F1700tsi", "test", "I", "test.I@netocentre.fr", 2, "0290009c")
-    eleveid_j = insert_fake_user(db, "F1700tsj", "test", "J", "test.J@netocentre.fr", 2, "0290009c")
-    eleveid_k = insert_fake_user(db, "F1700tsk", "test", "K", "test.K@netocentre.fr", 2, "0290009c")
-    eleveid_l = insert_fake_user(db, "F1700tsl", "test", "L", "test.L@netocentre.fr", 2, "0290009c")
-    eleveid_m = insert_fake_user(db, "F1700tsm", "test", "M", "test.M@netocentre.fr", 2, "0290009c")
-    temp["eleves"].extend([eleveid_a,eleveid_b,eleveid_c,eleveid_d,eleveid_e,eleveid_f,eleveid_g,eleveid_h,\
-    eleveid_i,eleveid_j,eleveid_k,eleveid_l,eleveid_m])
-
-    #Changement des dates de dernière connexions
-    update_lastlogin_user(db, eleveid_a, now - config.delete.delay_delete_student * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_b, now - config.delete.delay_delete_student * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_c, now - config.delete.delay_delete_student * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_d, now - config.delete.delay_anonymize_student * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_e, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_g, now - config.delete.delay_anonymize_student * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_h, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_i, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_j, now - config.delete.delay_anonymize_student * SECONDS_PER_DAY + 1)
-    update_lastlogin_user(db, eleveid_k, now)
-    update_lastlogin_user(db, eleveid_l, now)
-    update_lastlogin_user(db, eleveid_m, now)
-
-    #Mise à jour BD
-    db.connection.commit()
-
-    #Création d'un cours factice pour y inscire les éventuels utilisateurs
-    course_test1_id = webservice.create_course("testnettoyage", "testnettoyage", 1)[0]["id"]
-    temp["courses"].append(course_test1_id)
-
-    #Inscription des utilisateurs aux cours factices
-    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_a, course_test1_id)
-    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_d, course_test1_id)
-    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_k, course_test1_id)
-    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_j, course_test1_id)
-
-    #Création de fausses références dans des cours
-    refid_b = insert_fake_course_reference(db, eleveid_b)
-    refid_d = insert_fake_course_reference(db, eleveid_d)
-    refid_h = insert_fake_course_reference(db, eleveid_h)
-    refid_l = insert_fake_course_reference(db, eleveid_l)
-    temp["references"].extend([refid_b,refid_d,refid_h,refid_l])
-
-
-def insert_profs(db: Database, config: Config, arguments: Namespace, temp: dict[str, list[int]], webservice: WebService):
-    """
-    Insérère toutes les données nécéssaisres aux tests
-    des profs dans la base de données moodle
-    """
-    pass
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -187,92 +121,221 @@ def run_script(config: Config, arguments: Namespace):
         action_func(config, action, arguments)
 
 
+
+#--- TEST ELEVES ---#
+
 def test_eleve_anon_delete_delay_enrolled(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être supprimé
+    Elève qui ne s'est pas connecté depuis le délai pour être supprimé
     mais qui est inscrit à des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsa"))[10] == "Anonyme"
 
 def test_eleve_anon_delete_delay_references(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être supprimé
+    Elève qui ne s'est pas connecté depuis le délai pour être supprimé
     mais qui a des références dans des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsb"))[10] == "Anonyme"
 
 def test_eleve_delete_delete_delay_no_references_and_enrollements(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être supprimé
+    Elève qui ne s'est pas connecté depuis le délai pour être supprimé
     et qui n'a ni références ni inscriptions
     """
     assert db.get_user_data(db.get_user_id("F1700tsc")) == None
 
 def test_eleve_anon_anon_delay_references(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être anonymisé
+    Elève qui ne s'est pas connecté depuis le délai pour être anonymisé
     mais qui a des références dans des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsd"))[10] == "Anonyme"
 
 def test_eleve_delete_force_delay_enrolled(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être supprimé de force
+    Elève qui ne s'est pas connecté depuis le délai pour être supprimé de force
     mais qui a des inscriptions dans des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tse")) == None
 
 def test_eleve_delete_never_used(db):
     """
-    Utilisateur qui n'a jamais utilisé moodle
+    Elève qui n'a jamais utilisé moodle
     """
     assert db.get_user_data(db.get_user_id("F1700tsf")) == None
 
 def test_eleve_nothing_anon_delay_no_references_and_enrollements(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être anonymisé
+    Elève qui ne s'est pas connecté depuis le délai pour être anonymisé
     mais qui n'a pas de références ou d'inscriptions dans des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsg"))[11] == "G"
 
 def test_eleve_delete_force_delay_references(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être supprimé de force
+    Elève qui ne s'est pas connecté depuis le délai pour être supprimé de force
     mais qui a des références dans des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsh")) == None
 
 def test_eleve_delete_force_delay_no_references_and_enrollements(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être supprimé de force
+    Elève qui ne s'est pas connecté depuis le délai pour être supprimé de force
     et qui n'a ni références ni inscriptions
     """
     assert db.get_user_data(db.get_user_id("F1700tsi")) == None
 
 def test_eleve_anon_anon_delay_enrolled(db):
     """
-    Utilisateur qui ne s'est pas connecté depuis le délai pour être anonymisé
+    Elève qui ne s'est pas connecté depuis le délai pour être anonymisé
     mais qui est inscrit à des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsj"))[10] == "Anonyme"
 
 def test_eleve_nothing_no_delay_enrolled(db):
     """
-    Utilisateur qui s'est connecté avant les délais
+    Elève qui s'est connecté avant les délais
     et qui est inscrit à des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsk"))[11] == "K"
 
 def test_eleve_nothing_no_delay_references(db):
     """
-    Utilisateur qui s'est connecté avant les délais
+    Elève qui s'est connecté avant les délais
     et qui a des références dans des cours
     """
     assert db.get_user_data(db.get_user_id("F1700tsl"))[11] == "L"
 
 def test_eleve_nothing_no_delay_no_references_and_enrollements(db):
     """
-    Utilisateur qui s'est connecté avant les délais
+    Elève qui s'est connecté avant les délais
     mais qui n'est ni inscrit à des cours ni possède de références
     """
     assert db.get_user_data(db.get_user_id("F1700tsm"))[11] == "M"
+
+
+#--- TEST ENSEIGNANTS ---#
+
+#Pas de délai
+def test_enseignant_nothing_no_delay_enrolled(db):
+    """
+    Enseignant qui s'est connecté avant les délais et
+    qui est inscrit à des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700tta"))[11] == "A"
+
+def test_enseignant_nothing_no_delay_owner(db):
+    """
+    Enseignant qui s'est connecté avant les délais et
+    qui possède un cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttb"))[11] == "B"
+
+def test_enseignant_nothing_no_delay_references(db):
+    """
+    Enseignant qui s'est connecté avant les délais et
+    qui possède des références dans des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttc"))[11] == "C"
+
+def test_enseignant_nothing_no_delay_no_references_and_enrollements(db):
+    """
+    Enseignant qui s'est connecté avant les délais
+    mais qui n'a pas de références ni d'inscriptions
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttd"))[11] == "D"
+
+#Délais d'anonymisation
+def test_enseignant_anon_anon_delay_enrolled(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être anonymisé
+    et qui est inscrit à des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700tte"))[10] == "Anonyme"
+
+def test_enseignant_anon_anon_delay_owner(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être anonymisé
+    et qui possède un cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttf"))[10] == "Anonyme"
+
+def test_enseignant_anon_anon_delay_references(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être anonymisé
+    et qui possède des références dans des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttg"))[10] == "Anonyme"
+
+def test_enseignant_anon_anon_delay_no_references_and_enrollements(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être anonymisé
+    mais qui n'a pas de références ni d'inscriptions
+    """
+    assert db.get_user_data(db.get_user_id("F1700tth"))[11] == "H"
+
+#Délais de backup de cours
+# TODO: Tester les traitements sur les cours
+def test_enseignant_anon_backup_delay_enrolled(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour faire
+    un backup de ses cours et qui est inscrit à des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700tti"))[10] == "Anonyme"
+
+def test_enseignant_anon_backup_delay_owner(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour faire
+    un backup de ses cours et qui est possède un cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttj"))[10] == "Anonyme"
+
+def test_enseignant_anon_backup_delay_references(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour faire
+    un backup de ses cours et qui possède des références dans des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttk"))[10] == "Anonyme"
+
+def test_enseignant_anon_backup_delay_no_references_and_enrollements(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour faire un backup
+    de ses cours mais qui n'a pas de références ni d'inscriptions
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttl"))[11] == "H"
+
+#Délais de suppression
+def test_enseignant_anon_delete_delay_enrolled(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être supprimé
+    et qui est inscrit à des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttm"))[10] == "Anonyme"
+
+def test_enseignant_anon_delete_delay_owner(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être supprimé
+    et qui possède un cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttn"))[10] == "Anonyme"
+
+def test_enseignant_anon_delete_delay_references(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être supprimé
+    et qui possède des références dans des cours
+    """
+    assert db.get_user_data(db.get_user_id("F1700tto"))[10] == "Anonyme"
+
+def test_enseignant_delete_delete_delay_no_references_and_enrollements(db):
+    """
+    Enseignant qui ne s'est pas connecté depuis le délai pour être supprimé
+    mais qui n'a pas de références ni d'inscriptions
+    """
+    assert db.get_user_data(db.get_user_id("F1700ttp")) == None
+
+def test_enseignant_delete_never_used(db):
+    """
+    Enseignant qui n'a jamais utilisé moodle
+    """
+    assert db.get_user_data(db.get_user_id("F1700tsq")) == None
--- testv2/test_utils.py
+++ testv2/test_utils.py
@@ -1,4 +1,152 @@
 from synchromoodle.dbutils import Database
+from synchromoodle.config import Config
+from synchromoodle.webserviceutils import WebService
+from argparse import Namespace
+
+SECONDS_PER_DAY = 86400
+
+def insert_eleves(db: Database, config: Config, arguments: Namespace, temp: dict[str, list[int]], webservice: WebService):
+    """
+    Insérère toutes les données nécéssaisres aux tests
+    des élèves dans la base de données moodle
+    """
+    #Récupération du timestamp actuel
+    now = db.get_timestamp_now()
+
+    #Insertion des utilisateurs de test (élèves)
+    eleveid_a = insert_fake_user(db, "F1700tsa", "test", "A", "test.A@netocentre.fr", 2, "0290009c")
+    eleveid_b = insert_fake_user(db, "F1700tsb", "test", "B", "test.B@netocentre.fr", 2, "0290009c")
+    eleveid_c = insert_fake_user(db, "F1700tsc", "test", "C", "test.C@netocentre.fr", 2, "0290009c")
+    eleveid_d = insert_fake_user(db, "F1700tsd", "test", "D", "test.D@netocentre.fr", 2, "0290009c")
+    eleveid_e = insert_fake_user(db, "F1700tse", "test", "E", "test.E@netocentre.fr", 2, "0290009c")
+    eleveid_f = insert_fake_user(db, "F1700tsf", "test", "F", "test.F@netocentre.fr", 2, "0290009c")
+    eleveid_g = insert_fake_user(db, "F1700tsg", "test", "G", "test.G@netocentre.fr", 2, "0290009c")
+    eleveid_h = insert_fake_user(db, "F1700tsh", "test", "H", "test.H@netocentre.fr", 2, "0290009c")
+    eleveid_i = insert_fake_user(db, "F1700tsi", "test", "I", "test.I@netocentre.fr", 2, "0290009c")
+    eleveid_j = insert_fake_user(db, "F1700tsj", "test", "J", "test.J@netocentre.fr", 2, "0290009c")
+    eleveid_k = insert_fake_user(db, "F1700tsk", "test", "K", "test.K@netocentre.fr", 2, "0290009c")
+    eleveid_l = insert_fake_user(db, "F1700tsl", "test", "L", "test.L@netocentre.fr", 2, "0290009c")
+    eleveid_m = insert_fake_user(db, "F1700tsm", "test", "M", "test.M@netocentre.fr", 2, "0290009c")
+    temp["eleves"].extend([eleveid_a,eleveid_b,eleveid_c,eleveid_d,eleveid_e,eleveid_f,eleveid_g,eleveid_h,\
+    eleveid_i,eleveid_j,eleveid_k,eleveid_l,eleveid_m])
+
+    #Changement des dates de dernière connexions
+    update_lastlogin_user(db, eleveid_a, now - config.delete.delay_delete_student * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_b, now - config.delete.delay_delete_student * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_c, now - config.delete.delay_delete_student * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_d, now - config.delete.delay_anonymize_student * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_e, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_g, now - config.delete.delay_anonymize_student * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_h, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_i, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_j, now - config.delete.delay_anonymize_student * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, eleveid_k, now)
+    update_lastlogin_user(db, eleveid_l, now)
+    update_lastlogin_user(db, eleveid_m, now)
+
+    #Mise à jour BD
+    db.connection.commit()
+
+    #Création d'un cours factice pour y inscire les éventuels utilisateurs
+    course_test1_id = webservice.create_course("testnettoyage", "testnettoyage", 1)[0]["id"]
+    temp["courses"].append(course_test1_id)
+
+    #Inscription des utilisateurs aux cours factices
+    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_a, course_test1_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_d, course_test1_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_k, course_test1_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_eleve, eleveid_j, course_test1_id)
+
+    #Création de fausses références dans des cours
+    refid_b = insert_fake_course_reference_eleve(db, eleveid_b)
+    refid_d = insert_fake_course_reference_eleve(db, eleveid_d)
+    refid_h = insert_fake_course_reference_eleve(db, eleveid_h)
+    refid_l = insert_fake_course_reference_eleve(db, eleveid_l)
+    temp["references"].extend([refid_b,refid_d,refid_h,refid_l])
+
+
+def insert_profs(db: Database, config: Config, arguments: Namespace, temp: dict[str, list[int]], webservice: WebService):
+    """
+    Insérère toutes les données nécéssaisres aux tests
+    des profs dans la base de données moodle
+    """
+    #Récupération du timestamp actuel
+    now = db.get_timestamp_now()
+
+    #Insertion des utilisateurs de test (profs)
+    profid_a = insert_fake_user(db, "F1700tta", "testprof", "A", "testprof.A@netocentre.fr", 2, "0290009c")
+    profid_b = insert_fake_user(db, "F1700ttb", "testprof", "B", "testprof.B@netocentre.fr", 2, "0290009c")
+    profid_c = insert_fake_user(db, "F1700ttc", "testprof", "C", "testprof.C@netocentre.fr", 2, "0290009c")
+    profid_d = insert_fake_user(db, "F1700ttd", "testprof", "D", "testprof.D@netocentre.fr", 2, "0290009c")
+    profid_e = insert_fake_user(db, "F1700tte", "testprof", "E", "testprof.E@netocentre.fr", 2, "0290009c")
+    profid_f = insert_fake_user(db, "F1700ttf", "testprof", "F", "testprof.F@netocentre.fr", 2, "0290009c")
+    profid_g = insert_fake_user(db, "F1700ttg", "testprof", "G", "testprof.G@netocentre.fr", 2, "0290009c")
+    profid_h = insert_fake_user(db, "F1700tth", "testprof", "H", "testprof.H@netocentre.fr", 2, "0290009c")
+    profid_i = insert_fake_user(db, "F1700tti", "testprof", "I", "testprof.I@netocentre.fr", 2, "0290009c")
+    profid_j = insert_fake_user(db, "F1700ttj", "testprof", "J", "testprof.J@netocentre.fr", 2, "0290009c")
+    profid_k = insert_fake_user(db, "F1700ttk", "testprof", "K", "testprof.K@netocentre.fr", 2, "0290009c")
+    profid_l = insert_fake_user(db, "F1700ttl", "testprof", "L", "testprof.L@netocentre.fr", 2, "0290009c")
+    profid_m = insert_fake_user(db, "F1700ttm", "testprof", "M", "testprof.M@netocentre.fr", 2, "0290009c")
+    profid_n = insert_fake_user(db, "F1700ttn", "testprof", "N", "testprof.N@netocentre.fr", 2, "0290009c")
+    profid_o = insert_fake_user(db, "F1700tto", "testprof", "O", "testprof.O@netocentre.fr", 2, "0290009c")
+    profid_p = insert_fake_user(db, "F1700ttp", "testprof", "P", "testprof.P@netocentre.fr", 2, "0290009c")
+    profid_q = insert_fake_user(db, "F1700ttq", "testprof", "Q", "testprof.Q@netocentre.fr", 2, "0290009c")
+    temp["profs"].extend([profid_a,profid_b,profid_c,profid_d,profid_e,profid_f,profid_g,profid_h,\
+    profid_i,profid_j,profid_k,profid_l,profid_m,profid_n,profid_o,profid_p,profid_q])
+
+    #Changement des dates de dernière connexions
+    update_lastlogin_user(db, profid_a, now)
+    update_lastlogin_user(db, profid_b, now)
+    update_lastlogin_user(db, profid_c, now)
+    update_lastlogin_user(db, profid_d, now)
+    update_lastlogin_user(db, profid_e, now - config.delete.delay_anonymize_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_f, now - config.delete.delay_anonymize_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_g, now - config.delete.delay_anonymize_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_h, now - config.delete.delay_anonymize_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_i, now - config.delete.delay_backup_course * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_j, now - config.delete.delay_backup_course * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_k, now - config.delete.delay_backup_course * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_l, now - config.delete.delay_backup_course * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_m, now - config.delete.delay_delete_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_n, now - config.delete.delay_delete_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_o, now - config.delete.delay_delete_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_p, now - config.delete.delay_delete_teacher * SECONDS_PER_DAY + 1)
+    update_lastlogin_user(db, profid_p, now - config.delete.delay_force_delete * SECONDS_PER_DAY + 1)
+
+    #Mise à jour BD
+    db.connection.commit()
+
+    #Création de cours factices pour y inscire les éventuels utilisateurs
+    courseA_testprof_id = webservice.create_course("testnettoyageA", "testnettoyageA", 1)[0]["id"]
+    courseB_testprof_id = webservice.create_course("testnettoyageB", "testnettoyageB", 1)[0]["id"]
+    courseE_testprof_id = webservice.create_course("testnettoyageE", "testnettoyageE", 1)[0]["id"]
+    courseF_testprof_id = webservice.create_course("testnettoyageF", "testnettoyageF", 1)[0]["id"]
+    courseI_testprof_id = webservice.create_course("testnettoyageI", "testnettoyageI", 1)[0]["id"]
+    courseJ_testprof_id = webservice.create_course("testnettoyageJ", "testnettoyageJ", 1)[0]["id"]
+    courseM_testprof_id = webservice.create_course("testnettoyageM", "testnettoyageM", 1)[0]["id"]
+    courseN_testprof_id = webservice.create_course("testnettoyageN", "testnettoyageN", 1)[0]["id"]
+    temp["courses"].extend([courseA_testprof_id,courseB_testprof_id,courseE_testprof_id,courseF_testprof_id,\
+    courseI_testprof_id,courseJ_testprof_id,courseM_testprof_id,courseN_testprof_id])
+
+    #Inscription des utilisateurs aux cours factices
+    #Inscriptions simples en tant qu'enseignant
+    webservice.enrol_user_to_course(config.constantes.id_role_enseignant, profid_a, courseA_testprof_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_enseignant, profid_e, courseE_testprof_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_enseignant, profid_i, courseI_testprof_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_enseignant, profid_m, courseM_testprof_id)
+    #Inscriptions en tant que propriétaire de cours
+    webservice.enrol_user_to_course(config.constantes.id_role_proprietaire_cours, profid_b, courseB_testprof_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_proprietaire_cours, profid_f, courseF_testprof_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_proprietaire_cours, profid_j, courseJ_testprof_id)
+    webservice.enrol_user_to_course(config.constantes.id_role_proprietaire_cours, profid_n, courseN_testprof_id)
+
+    #Création de fausses références dans des cours
+    refidprof_c = insert_fake_course_reference_enseignant(db, profid_c)
+    refidprof_g = insert_fake_course_reference_enseignant(db, profid_g)
+    refidprof_k = insert_fake_course_reference_enseignant(db, profid_k)
+    refidprof_o = insert_fake_course_reference_enseignant(db, profid_o)
+    temp["references"].extend([refidprof_c,refidprof_g,refidprof_k,refidprof_o])
+
 
 def insert_fake_user(db: Database, username: str, first_name: str, last_name: str, email: str, mail_display: int, theme: str):
     """
@@ -15,7 +163,17 @@ def remove_fake_user(db: Database, userid: int):
     s = "DELETE FROM {entete}user WHERE id = %(userid)s".format(entete=db.entete)
     db.mark.execute(s, params={'userid': userid})
 
-def insert_fake_course_reference(db: Database, userid: int):
+def insert_fake_course_reference_eleve(db: Database, userid: int):
+    """
+    Fonction permettant de donner une référence à un utilisateur dans un cours
+    On ne se préoccupe pas des dépendances car ici la table de l'historique des notes
+    peut de toute manière faire référence à des cours qui n'éxistent plus
+    """
+    s = "INSERT INTO {entete}grade_grades_history (action, oldid, source, timemodified, loggeduser, itemid, userid, rawgrade, rawgrademax, rawgrademin, rawscaleid, usermodified, finalgrade, hidden, locked, locktime, exported, overridden, excluded, feedback, feedbackformat, information, informationformat) VALUES (1, 0, 'mod/assign', 0, 0, 0, %(userid)s, 50, 100.00000, 0.00000, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, NULL, 0, NULL, 0)".format(entete=db.entete)
+    db.mark.execute(s, params={'userid': userid})
+    return db.mark.lastrowid
+
+def insert_fake_course_reference_enseignant(db: Database, userid: int):
     """
     Fonction permettant de donner une référence à un utilisateur dans un cours
     On ne se préoccupe pas des dépendances car ici la table de l'historique des notes
